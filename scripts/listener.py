121698f49851fb1d803d712d10c5580f876e9143
Glenn <glenn+snow_flake@mercury-wireless.com>
2018-01-10 21:22:13 -0500

    Adding statsd clients

---
+++ scripts/listener.py
@@ -0,0 +1,16 @@
+# From https://wiki.python.org/moin/UdpCommunication modified for Statsd + debugging usage
+import datetime
+import socket
+
+UDP_IP = "127.0.0.1"
+UDP_PORT = 8080
+
+sock = socket.socket(
+    socket.AF_INET,     # Internet
+    socket.SOCK_DGRAM,  # UDP
+)
+sock.bind((UDP_IP, UDP_PORT))
+
+while True:
+    data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
+    print(datetime.datetime.now(), data)
