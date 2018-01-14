5f62ae84ca8efaa14f570203f742e28b19381844
snow-flake <30215944+snow-flake@users.noreply.github.com>
2018-01-14 11:36:32 -0500

    Statsd (#2)
    * Adding statsd clients
    
    * Added statsd client and test app

---
+++ includes/statsd_test.cpp
@@ -0,0 +1,29 @@
+//
+// Created by Snow Flake on 1/10/18.
+//
+
+#include "StatsdClient.hpp"
+
+int main()
+{
+    // Define the client on localhost, with port 8080, using a prefix and a batching size of 20 bytes
+	Statsd::StatsdClient client{ "127.0.0.1", 8080, "myPrefix.", 20 };
+
+    // Increment the metric "coco"
+    client.increment("coco");
+
+    // Decrement the metric "kiki"
+    client.decrement("kiki");
+
+    // Adjusts "toto" by +3
+    client.count("toto", 2, 0.1f);
+
+    // Record a gauge "titi" to 3
+    client.gauge("titi", 3);
+
+    // Record a timing of 2ms for "myTiming"
+    client.timing("myTiming", 2, 0.1f);
+
+    // Send a metric explicitly
+    client.send("tutu", 4, "c", 2.0f);
+}
