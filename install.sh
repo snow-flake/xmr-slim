d9b38973bcbc4704981308ea7c3ffe5c94545976
Glenn <glenn+snow_flake@mercury-wireless.com>
2018-01-14 11:35:40 -0500

    Added installer to bootstrap the system

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+sudo apt-get update
+sudo apt-get install  build-essential gcc-5 g++-5 make cmake g++ gcc-6 -y
+
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+
+sudo apt update
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 1 --slave /usr/bin/g++ g++ /usr/bin/g++-5
+
+rm -rf cmake-3.10.1*
+wget https://cmake.org/files/v3.10/cmake-3.10.1.tar.gz
+tar -xvf cmake-3.10.1.tar.gz 
+cd ./cmake-3.10.1 && ./bootstrap && make && make install
+
+sudo update-alternatives --install /usr/bin/cmake cmake /usr/local/bin/cmake 1 --force
+
+sudo apt install -y libssl-dev libhwloc-dev
+
