909e33853bee1a2632be3862ce051d45b8e96b28
snow-flake <30215944+snow-flake@users.noreply.github.com>
2018-01-23 21:54:56 -0500

    Moved self-test out of minerd and into c_cryptonight (#25)

---
+++ c_cryptonight/minethed_self_test_main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Snow Flake on 1/23/18.
+//
+
+#include "minethed_self_test.h"
+
+int main() {
+	return minethed_self_test::test_func_selector() && minethed_self_test::test_func_multi_selector();
+}
