5964d4257de190ffaa83780d4e9fcaa04f6c46d3
Glenn <glenn+snow_flake@mercury-wireless.com>
2018-01-25 19:06:17 -0500

    Moving auto-adjust out of runtime

---
+++ sys_auto_adjust/autoAdjust.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <unistd.h>
+#include <vector>
+#include <cstdint>
+
+
+// define xmr settings
+#define MONERO_MEMORY 2097152llu
+#define MONERO_MASK 0x1FFFF0
+#define MONERO_ITER 0x80000
+
+
+namespace xmrstak
+{
+	namespace cpu {
+
+		struct auto_thd_cfg {
+			int low_power_mode;
+			long long affine_to_cpu;
+		};
+
+		class auto_threads {
+		public:
+
+			const size_t hashMemSize;
+			const size_t halfHashMemSize;
+			const uint32_t processors_count;
+			const uint32_t cache_l2;
+			const uint32_t cache_l3;
+			std::vector<auto_thd_cfg> configs;
+
+			auto_threads();
+
+		private:
+			bool is_old_amd(int32_t *cpu_info);
+		};
+
+	}
+}
